187e75fb015bb3b9b04dbec3d46f875465e7f65f
Yingxi Kong <you@example.com>
2023-12-08 00:50:25 -0500

    retire plan function update

--- app.R
+++ app.R
@@ -7,6 +7,37 @@
 #    http://shiny.rstudio.com/
 #
 
+retire_plan <- function(D, x, age, current_year, retire_year, i, p) {
+  col_names <- c("year","age", "profile_value")
+  profile_df <- data.frame(matrix(ncol=length(col_names), nrow=0))
+  colnames(profile_df) <- col_names
+  n <- retire_year-current_year
+  before_retire <- data.frame(matrix(ncol=length(col_names), nrow=0))
+  colnames(before_retire) <- col_names
+  before_retire$year <- seq(current_year, retire_year, 1)
+  before_retire$age <- seq(age, age+n, 1)
+  after_retire <- data.frame(matrix(ncol=length(col_names), nrow=0))
+  colnames(before_retire) <- col_names
+  end_year <- current_year+(100-age)
+  after_retire$year <- seq(retire_year+1, end_year, 1)
+  after_retire$age <- seq(age+n+1, 100, 1)
+  value_list <- c()
+  
+  for (j in 0:n){
+    current_value <- D(1+i)^n+x*(((1+i)^n-1)/i)
+    value_list <- c(value_list, current_value)
+  }
+  before_retire$profile_value <- value_list
+  profile_df <- rbind(profile_df, before_retire)
+  
+  
+  
+  
+  
+  
+}
+}
+}
 
 library(shiny)
 
@@ -18,18 +49,20 @@ ui <- fluidPage(
   # Adding numeric input field for the number of simulation; Adding radio buttons for selecting the result type.
   sidebarLayout(
     sidebarPanel(
-      numericInput("num_simulations", "Number of Simulations:", value = 100),
-      radioButtons("typeInput", "Result type",
-                   choices = c("Win", "Lose", "Draw", "All"),
-                   selected = "All"),
+      sliderInput("down_pmt", "Down Payment:", min=0, max=1000000, value=100000),
+      sliderInput("annual_pmt", "Annual Payment:", min=0, max=50000, value=10000),
+      sliderInput("social_income", "Social Security Income:", min=0, max=50000, value=10000),
+      sliderInput("annual_spending", "Annual Spending:", min=0, max=100000, value = 50000),
+      numericInput("age", "Current Age:", value = 30),
+      numericInput("retire_yr", "Retire Year:", min=2023, max= 2123, value = 2030),
       actionButton("simulate", "Simulate")                 # action button to start the simulation
     ),
     
     # Adding a main panel with a title, plot output, and table output.
     mainPanel(
-      actionButton("sim_desc", "Tell me about the simulation"),
+      actionButton("sim_desc", "Tell me about my Retirement Plan"),
       h3("Simulation Results"),
-      plotOutput("win_lose_histogram"),
+      plotOutput("prortfolio_value"),
       htmlOutput("results_table")
     )
   )
