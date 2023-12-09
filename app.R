d630f1d6c7c9cd41d70d15cd8e0f402ca36002df
Yingxi Kong <you@example.com>
2023-12-08 22:53:22 -0500

    plot update

--- app.R
+++ app.R
@@ -1,50 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(tidyverse)
+library(shiny)
 
-#Retirement Plan Function
-retire_plan <- function(D, x, age, current_year, retire_year, i, p) {
-  #create a data frame that stores year, age and profile value
-  col_names <- c("year","age", "profile_value")
-  profile_df <- data.frame(matrix(ncol=length(col_names), nrow=0))
-  colnames(profile_df) <- col_names
+retire_plan <- function(D, x, age, current_year, retire_year, s, i, p) {
+  # Calculate the number of years between current year and retirement
+  n <- retire_year - current_year
   
-  #calculate the year between retirement and current 
-  n <- retire_year-current_year
+  # Create a data frame for before retirement
+  before_retire <- data.frame(
+    year = seq(current_year, retire_year, by = 1),
+    age = seq(age, age + n, by = 1),
+    profile_value = numeric(n + 1)
+  )
   
-  #create a data frame that stores year, age and profile value before retirment
-  before_retire <- data.frame(matrix(ncol=length(col_names), nrow=0))
-  colnames(before_retire) <- col_names
-  before_retire$year <- seq(current_year, retire_year, 1)
-  before_retire$age <- seq(age, age+n, 1)
-  
-  #create a data frame that stores year, age and profile value after retirment
-  after_retire <- data.frame(matrix(ncol=length(col_names), nrow=0))
-  colnames(before_retire) <- col_names
-  end_year <- current_year+(100-age)
-  after_retire$year <- seq(retire_year+1, end_year, 1)
-  after_retire$age <- seq(age+n+1, 100, 1)
-  
-  #create a vector to store profile values
-  value_list <- c()
-  
-  #loop from current year to retirement year
-  for (j in 0:n){
-    #calculate each year's profile value and add to value_list
-    current_value <- D(1+i)^n+x*(((1+i)^n-1)/i)
-    value_list <- c(value_list, current_value)
+  # Calculate profile values before retirement
+  for (j in 0:n) {
+    before_retire$profile_value[j + 1] <- D * (1 + i)^j + x * (((1 + i)^j - 1) / i)
   }
-  #combine before_retire data frame to the profile_df data frame
-  before_retire$profile_value <- value_list
-  profile_df <- rbind(profile_df, before_retire)
+  
+  # Create a data frame for after retirement
+  end_year <- current_year + (100 - age)
+  m <- end_year - retire_year
+  after_retire <- data.frame(
+    year = seq(retire_year + 1, end_year, by = 1),
+    age = seq(age + n + 1, 100, by = 1),
+    profile_value = numeric(m)
+  )
+  
+  # Calculate profile values after retirement
+  current_value <- before_retire$profile_value[n + 1]
+  for (j in 1:m) {
+    current_value <- current_value * (1 + i) - p + s
+    after_retire$profile_value[j] <- current_value
+  }
+  
+  # Combine the two data frames
+  profile_df <- rbind(before_retire, after_retire)
+  return(profile_df)
+}
+#simulation function
+sim_retirement <- function(D, x, age, current_year, retire_year, s, p, interest_list,num_simulations){
+  simulate_result <- data.frame()
+  interest_list <- sample(seq(0.01, 0.1, by=0.0005), num_simulations, replace=T)
+  for(l in 1:length(interest_list)){
+    result <- retire_plan(D, x, age, current_year, retire_year, s, interest_list[l], p)
+    result <- result %>%
+      mutate(interest_rate=rep(interest_list[l],nrow(result))) %>%
+      mutate(sim_num=rep(l,nrow(result))) %>%
+      mutate(event=case_when(result[nrow(result),3] >=0 ~ "Success",
+                             TRUE ~ "Failure"))
+    simulate_result <- rbind(simulate_result, result)
+  }
+  return(simulate_result)
 }
 
-library(shiny)
+
 # Define UI for application 
 ui <- fluidPage(
   # Application title
@@ -53,11 +62,13 @@ ui <- fluidPage(
   # Adding numeric input field for the number of simulation; Adding radio buttons for selecting the result type.
   sidebarLayout(
     sidebarPanel(
+      numericInput("num_simulations", "Number of Simulations:", value = 1000),
       sliderInput("down_pmt", "Down Payment:", min=0, max=1000000, value=100000),
       sliderInput("annual_pmt", "Annual Payment:", min=0, max=50000, value=10000),
       sliderInput("social_income", "Social Security Income:", min=0, max=50000, value=10000),
       sliderInput("annual_spending", "Annual Spending:", min=0, max=100000, value = 50000),
       numericInput("age", "Current Age:", value = 30),
+      numericInput("current_yr", "Current Year:", min=2023, max= 2123, value = 2023),
       numericInput("retire_yr", "Retire Year:", min=2023, max= 2123, value = 2030),
       actionButton("simulate", "Simulate")                 # action button to start the simulation
     ),
@@ -66,25 +77,70 @@ ui <- fluidPage(
     mainPanel(
       actionButton("sim_desc", "Tell me about my Retirement Plan"),
       h3("Simulation Results"),
-      plotOutput("prortfolio_value"),
-      htmlOutput("results_table")
+      plotOutput("portfolio_value"),
+      tableOutput("results_table")
     )
   )
 )
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
+  # Pop-up text for the simulation description
+  observeEvent(input$sim_desc, {
+    showModal(modalDialog(title = "Retirement Planning Simulation Description", 
+                          HTML("In this simulation,")))
   })
+  
+  
+  # Simulated results preparation - will be reactive when a new simulation is run
+  simulated_results <- eventReactive(input$simulate, {
+    num_simulations <- input$num_simulations
+    D <- input$down_pmt 
+    x <- input$annual_pmt 
+    age <- input$age 
+    current_year <- input$current_yr
+    retire_year <- input$retire_yr
+    s <- input$social_income 
+    p <- input$annual_spending 
+    simulate_result <- sim_retirement(D, x, age, current_year, retire_year, s, p, interest_list,num_simulations)
+    
+    return(simulate_result)
+  })
+  
+  #create a plot
+  output$portfolio_value <- renderPlot({
+    # Get the results from the simulation
+    results <- simulated_results()
+    retire_year <- input$retire_yr
+    # Plot the results
+    p <- ggplot(results, aes(x = year, y = profile_value, group = sim_num)) +
+      geom_line(aes(color = factor(event)), alpha = 1, size = 0.1) +
+      theme_minimal() +
+      labs(
+        x = "Year",
+        y = "Portfolio Value",
+        color = "Event Status"
+      ) +
+      geom_vline(xintercept = retire_year, linetype = "dashed", color = "black") +
+      annotate("text", x = retire_year+10, y = 4900000, label = paste("Retire Year:", retire_year), hjust = 1) +
+      scale_color_manual(values = c("Success" = "chartreuse3", "Failure" = "orangered1")) +
+      coord_cartesian(ylim = c(0, 5000000), xlim = c(min(results$year), retire_year+30))
+    
+    p
+  })
+  
+  # Create a table
+  results_table <- reactive({
+    results <- simulated_results()
+    table <- head(results)
+    table
+  })
+  output$results_table <- renderTable({
+    results_table()
+  })
+  
+  
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
