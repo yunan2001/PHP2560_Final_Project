b69d340f90a43738c55651e1edfa3946c548c827
yunan2001 <153023742+yunan2001@users.noreply.github.com>
2023-12-12 20:09:59 -0500

    # 12/12 biubiu Function

--- app.R
+++ app.R
@@ -1,9 +1,16 @@
 library(tidyverse)
 library(shiny)
 library(plotly)
+library(shinythemes)
+library(shinycssloaders)
+library(htmltools)
+library(bsplus)
+library(shinyBS)
+library(shinydashboard)
+library(shinyWidgets)
 
 retire_plan <- function(D, x, age, current_year, retire_year, s, i, r, p) {
-  #' Returns a data frame of result from a single simulation for the profile value of user
+  #' Returns a data frame of result from a single simulation for the portfolio value of user
   #' @param D, the amount of down payment the user wants to put
   #' @param x, the amount of annual payment the user wants to put
   #' @param age, the age of the user
@@ -13,7 +20,7 @@ retire_plan <- function(D, x, age, current_year, retire_year, s, i, r, p) {
   #' @param i, the return rate of the retirement plan
   #' @param r, the inflation rate in the following years
   #' @param p, the amount of money the user expected to spend annually after retirement
-  #' @return a data frame containing year, age, and profile value for each year
+  #' @return a data frame containing year, age, and portfolio value for each year
   
   # Calculate the number of years between current year and retirement
   n <- retire_year - current_year
@@ -22,12 +29,12 @@ retire_plan <- function(D, x, age, current_year, retire_year, s, i, r, p) {
   before_retire <- data.frame(
     year = seq(current_year, retire_year, by = 1),
     age = seq(age, age + n, by = 1),
-    profile_value = numeric(n + 1)
+    portfolio_value = numeric(n + 1)
   )
   
-  # Calculate profile values before retirement
+  # Calculate portfolio values before retirement
   for (j in 0:n) {
-    before_retire$profile_value[j + 1] <- D * (1 + (i-r)/(1+r))^j + x * (((1 + (i-r)/(1+r))^j - 1) / (i-r)/(1+r))
+    before_retire$portfolio_value[j + 1] <- D * (1 + (i-r)/(1+r))^j + x * (((1 + (i-r)/(1+r))^j - 1) / (i-r)/(1+r))
   }
   
   # Create a data frame for after retirement
@@ -36,25 +43,25 @@ retire_plan <- function(D, x, age, current_year, retire_year, s, i, r, p) {
   after_retire <- data.frame(
     year = seq(retire_year + 1, end_year, by = 1),
     age = seq(age + n + 1, 100, by = 1),
-    profile_value = numeric(m)
+    portfolio_value = numeric(m)
   )
   
-  # Calculate profile values after retirement
-  current_value <- before_retire$profile_value[n + 1]
+  # Calculate portfolio values after retirement
+  current_value <- before_retire$portfolio_value[n + 1]
   for (j in 1:m) {
     current_value <- as.integer(current_value * (1 + (i-r)/(1+r)) - p + s)
-    after_retire$profile_value[j] <- current_value
+    after_retire$portfolio_value[j] <- current_value
   }
   
   # Combine the two data frames
-  profile_df <- rbind(before_retire, after_retire)
-  return(profile_df)
+  portfolio_df <- rbind(before_retire, after_retire)
+  return(portfolio_df)
 }
 
 
 #simulation function
 sim_retirement <- function(D, x, age, current_year, retire_year, s, p, interest_list, infla_list, num_simulations){
-  #' Returns a data frame of result from all simulations for the profile value of user
+  #' Returns a data frame of result from all simulations for the portfolio value of user
   #' @param D, the amount of down payment the user wants to put
   #' @param x, the amount of annual payment the user wants to put
   #' @param age, the age of the user
@@ -65,7 +72,7 @@ sim_retirement <- function(D, x, age, current_year, retire_year, s, p, interest_
   #' @param interest_list, a list of sampled return rate for the retirement plan
   #' @param infla_list, a list of sampled inflation rate in the following years
   #' @param num_simulations, the number of simulation the user wants to perform
-  #' @return a data frame containing year, age, profile value for each year, 
+  #' @return a data frame containing year, age, portfolio value for each year, 
   #' number of simulation, and result for all simulations
   
   #set up an empty data frame
@@ -109,28 +116,44 @@ sim_retirement <- function(D, x, age, current_year, retire_year, s, p, interest_
 
 
 ui <- fluidPage(
+  theme=shinytheme("sandstone"),
   titlePanel("Retirement Planning Simulator"),
   actionButton("sim_desc", "How Does This Retirement Plan Simulator Work"),
   h3("Tell Me About Your Plan"),
   sidebarLayout(
     sidebarPanel(
+      tags$style(
+        HTML(".tooltip {
+                        width: 400px;
+                        text-color:black;
+                        background-color:white;
+                        }")),
       numericInput("num_simulations", "Number of Simulations:", value = 1000),
-      sliderInput("down_pmt", "Down Payment:", 
-                  min = 0, max = 1000000, 
-                  value = 500000, 
-                  round=TRUE,step=5000),
+      sliderInput("down_pmt", "Down Payment:",
+                  min = 0, max = 1000000,
+                  value = 500000,
+                  round=TRUE,step=5000
+                  ),
+      bsTooltip("down_pmt", "The initial lump sum you're investing", placement = "right"),
+     
       sliderInput("annual_pmt", "Annual Payment:", 
                   min = 0, max = 100000, 
                   value = 30000, 
                   round=TRUE,step=5000),
-      sliderInput("social_income", "Social Security Income:", 
+      bsTooltip("annual_pmt", title="How much you'll add to your savings each year", placement = "right"),
+      
+      sliderInput("social_income", "Social Security Benefit:", 
                   min = 0, max = 100000, 
                   value = 20000, 
                   round=TRUE,step=5000),
-      sliderInput("annual_spending", "Annual Spending:", 
+      bsTooltip("social_income", "The yearly income you expect from Social Security after retirement", placement = "right"),
+      
+      sliderInput("annual_spending", "Annual Investment:", 
                   min = 0, max = 100000, 
                   value = 30000, 
                   round=TRUE,step=5000),
+      bsTooltip("annual_spending", "Your estimated yearly living costs", placement = "right"),
+      
       numericInput("age", "Your Age:", value = 30),
       numericInput("current_yr", "Plan Start Year:", 
                    min = 2023, max = 2123, 
@@ -138,19 +161,46 @@ ui <- fluidPage(
       numericInput("retire_yr", "Expected Retire Year:", 
                    min = 2023, max = 2123, 
                    value = 2050),
-      actionButton("simulate", "Simulate")
+      actionButton("simulate", "Simulate"), 
     ),
     mainPanel(
       tabsetPanel(type = "tabs",
-                  tabPanel("Plot", plotlyOutput("portfolio_value"), 
-                           div(plotlyOutput("Guage_prob", 
-                                            width = "400px", 
-                                            height = "400px"), 
-                               align = "center")),
-                  tabPanel("Table", tableOutput("results_table"))
+                  tabPanel("Visualization",
+                           div(plotlyOutput("portfolio_value")%>% 
+                                 withSpinner(), br(), 
+                               div(textOutput("plot_text"),style = "font-size: 18px;"), br(), 
+                               div(plotlyOutput("Guage_prob", width = "400px",height = "400px"), align = "center"), br(), 
+                               div(textOutput("guage_text"), style = "font-size: 18px;")) ),
+                  tabPanel("Summary Table", tableOutput("results_table")
+                           ,
+                           br(),
+                           div(textOutput("intro_table")%>% withSpinner(),
+                               style = "font-size: 18px;")
+                  )
+                  
+                  # tabPanel("Visualization", 
+                  #          plotlyOutput("portfolio_value") 
+                  #          %>% withSpinner(), 
+                  #          br(),
+                  #          div(textOutput("plot_text"), 
+                  #              style = "font-size: 18px;"),
+                  #          div(plotlyOutput("Guage_prob", 
+                  #                           width = "400px", 
+                  #                           height = "400px"), 
+                  #              align = "center"),
+                  #          br(),
+                  #          div(textOutput("guage_text"), 
+                  #              style = "font-size: 18px;")),
+                  # tabPanel("Summary Table", tableOutput("results_table")
+                  #          %>% withSpinner(),
+                  #          br(),
+                  #          div(textOutput("intro_table"), 
+                  #              style = "font-size: 18px;")
+                  # )
+      )
     )
   )
-))
+)
 
 
 server <- function(input, output) {
@@ -158,7 +208,15 @@ server <- function(input, output) {
   #Pop-up text for the simulation description
   observeEvent(input$sim_desc, {
     showModal(modalDialog(title = "Retirement Planning Simulation Description", 
-                          HTML("In this simulation,")))
+                          HTML("Welcome to our easy-to-use Retirement Planning Simulator! This interactive tool is here to help you figure out how long your retirement savings could last. Let's see if your funds from your retirement investment plan can support you up to age 100. Just follow a few simple steps to get started. Start by setting the number of simulations - remember, more simulations can lead to more precise outcomes. Then, provide the following information to customize your retirement plan: 
+<p><p> <b>Down Payment</b>: The initial lump sum you're investing.
+<p> <b>Annual Investment</b>: How much you'll add to your savings each year.
+<p> <b>Social Security Benefit</b>: The yearly income you expect from Social Security after retirement.
+<p> <b>Annual Spending</b>: Your estimated yearly living costs.
+<p> <b>Your Age</b>: Your current Age
+<p> <b>Plan Start Year</b>: Year You Plan to Start Investing
+<p> <b>Expected Retire Year</b>: Year You Plan to Retire
+<p> Fill in these details and hit 'Simulate' to map out your financial forecast! You can reset your financial inputs to find your customized optimal plan that can support you up to age 100!")))
   })
   
   #Simulated results preparation - will be reactive when a new simulation is run
@@ -188,10 +246,10 @@ server <- function(input, output) {
     retire_year <- input$retire_yr
     
     #Plot the results
-    p <- ggplot(results, aes(x = year, y = profile_value, group = sim_num)) +
+    p <- ggplot(results, aes(x = year, y = portfolio_value, group = sim_num)) +
       geom_line(aes(color = factor(event), 
-                    text = paste0("Profile Value:$", 
-                                  prettyNum(profile_value,big.mark = ","),
+                    text = paste0("portfolio Value:$", 
+                                  prettyNum(portfolio_value,big.mark = ","),
                                   '\n', "Year:", year)),
                 alpha = 1, size = 0.1) +
       theme_minimal() +
@@ -212,20 +270,29 @@ server <- function(input, output) {
     fig <- ggplotly(p, tooltip = "text")
   })
   
+  #print the description of line plot
+  plot_text <- eventReactive(input$simulate, {
+    "This interactive chart displays the value of your retirement savings year by year, starting from when you first contribute to when you reach 100 years old. If the line is green, your savings are on track to last until you're 100. A red line means your funds might run out sooner. Click on any part of the line to see the exact value of your savings for that year."
+    })
+  
+  output$plot_text <- renderText({
+    plot_text()
+  })
+  
   #Create a guage
   output$Guage_prob <- renderPlotly({
     
     #Get the results from the simulation
     results <- simulated_results()
     
-    #calculate the probability of success and failure trials, and the average profile value
+    #calculate the probability of success and failure trials, and the average portfolio value
     prob_table <- results %>%
       filter(year == max(year)) %>%
       group_by(event) %>%
       summarize(
         count = n(),
         prob = count / input$num_simulations,
-        mean_profile = mean(profile_value))
+        mean_portfolio = mean(portfolio_value))
     
     #convert the probability to percentage and set the range for different level
     prob_success <- 100*prob_table$prob[prob_table$event == "Success"]
@@ -260,14 +327,22 @@ server <- function(input, output) {
     fig <- fig %>% layout(margin = list(l=30, r=30, t=80, b=30))
   })
   
+  #print the description of gauge
+  guage_text <- eventReactive(input$simulate, {
+    "The gauge meter below shows your 'success probability' – that's your chance of having enough money through age 100. Aim for the gauge to show at least an 80% success rate for a more secure financial future!" 
+  })
+  output$guage_text <- renderText({
+    guage_text()
+  })
+  
   # Create a table
   results_table <- reactive({
     
     #Get the results from the simulation
     results <- simulated_results()
     
-    #convert all negative profile value to 0
-    results$profile_value <- pmax(results$profile_value, 0)
+    #convert all negative portfolio value to 0
+    results$portfolio_value <- pmax(results$portfolio_value, 0)
     
     #filter all rows which have year in the years list
     years <- c(input$current_yr+5, 
@@ -280,11 +355,11 @@ server <- function(input, output) {
     results_filter <- results %>%
       filter(year %in% years)
     
-    #find the quantile of each year's profile value and combine all results together to get the table
+    #find the quantile of each year's portfolio value and combine all results together to get the table
     quantile_df <- data.frame(Trials=c("Best", "25th Percentile", "50th Percentile", "75th Percentile", "Worst"))
     
     for (x in years) {
-      year_data <- results_filter[results_filter$year==x, "profile_value"]
+      year_data <- results_filter[results_filter$year==x, "portfolio_value"]
       year_quantiles <- round(quantile(year_data, probs = c(1, 0.75, 0.5, 0.25, 0)), 0)
       year_quantiles_df <- setNames(as.data.frame(year_quantiles), paste("Year", x))
       quantile_df <- cbind(quantile_df, year_quantiles_df) 
@@ -298,10 +373,11 @@ server <- function(input, output) {
     #find the average year for our retirement plan running out of money and compile it to our df
     year_go0_df <- results %>% 
       group_by(sim_num) %>%
-      filter(profile_value==0) %>%
+      filter(portfolio_value==0) %>%
       slice_min(year)
     
     mean_year_go0 <- round(mean(year_go0_df$year), 0)
+    mean_year_go0 <- ifelse(is.nan(mean_year_go0), 0, mean_year_go0)
     
     quantile_df <- quantile_df %>%
       mutate(mean_year=c("", "", "", "", mean_year_go0))
@@ -319,6 +395,15 @@ server <- function(input, output) {
     results_table()
   })
   
+  #print the description of table
+  table_text <- eventReactive(input$simulate, {
+    "This summary table exhibits the simulated result of portfolio value. It identifies the optimal, 75th, median (50th), 25th percentile, and lowest portfolio values across different years. The additional column on the last reveals the average year when the portfolio value depletes to zero. In addition, a zero indicates you have a perfectly successful plan that your funds are sufficient to last until the age of 100." 
+  })
+  output$intro_table <- renderText({
+    table_text()
+  })
+  
 }
+
 # Run the application 
 shinyApp(ui = ui, server = server)
